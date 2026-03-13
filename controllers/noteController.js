421c8ebf22045a6cdd4c87e0144a124336042954
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-13 10:26:04 +0530

    refactor: noteController updated

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,51 +1,28 @@
 const noteService = require('../services/noteService');
+const asyncHandler = require('../utils/asyncHandler');
 
-exports.createItem = async (req, res, next) => {
-    try {
+exports.createItem = asyncHandler(async(req, res) => {
         const note = await noteService.createItem(req.body);
         res.status(201).json(note);
-    }
-    catch (err) {
-        next(err); //Skip normal middleware Go to error-handling middleware
-    }
-}
+});
 
-exports.getItems = async (req, res, next) => {
-    try {
+exports.getItems = asyncHandler(async (req, res) => {
         const notes = await noteService.getItems();
         res.status(200).json(notes);
-    }
-    catch(err) {
-        next(err);
-    }
-}
+});
 
-exports.getItemById = async (req, res, next) => {
-    try {
-        const note = await noteService.getItemById(req.params.id);
-        res.status(200).json(note);
-    } 
-    catch(err) {
-        next(err);
-    }
-}
+exports.getItemById = asyncHandler(async (req, res) => {
+    const note = await noteService.getItemById(req.params.id);
+    res.status(200).json(note);
+});
 
-exports.updateItem = async (req, res, next) => {
-    try {
-        const note = await noteService.updateItem(req.params.id, req.body);
-        res.status(201).json(note);
-    }
-    catch(err) {
-        next(err);
-    }
-}
+exports.updateItem = asyncHandler(async (req, res) => {
+    const note = await noteService.updateItem(req.params.id, req.body);
+    res.status(201).json(note);
+});
+   
 
-exports.deleteItem = async (req, res, next) => {
-    try {
-        await noteService.deleteItem(req.params.id);
-        res.status(204).json("Note deleted successfully");
-    }
-    catch(err) {
-        next(err);
-    }
-}
+exports.deleteItem = asyncHandler(async (req, res) => {
+    await noteService.deleteItem(req.params.id);
+    res.status(204).json("Note deleted successfully");
+});
