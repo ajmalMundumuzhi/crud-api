5ecf0f83beb8f209840b097a61ebabb5190a0396
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-17 12:32:20 +0530

    refactor: updated redis for render deployment

--- config/redis.js
+++ config/redis.js
@@ -1,17 +1,37 @@
-const { createClient } = require('redis');
-const logger = require('../utils/logger');
-const { redis_uri } = require('./env');
+const { createClient } = require("redis");
 
-const redisClient = createClient({
-    url: redis_uri,
-})
+let redisClient;
 
-redisClient.on("connect", () => {
-    logger.info("Redis Connected Successfully");
-})
-
-redisClient.on("error", (err) => {
-    logger.error("Redis Connection Error: ", err);
-});
+if (process.env.NODE_ENV !== "production") {
+    redisClient = createClient({
+        url: process.env.REDIS_URI
+    });
+} else {
+    redisClient = {
+        get: async () => null,
+        set: async () => null,
+        del: async () => null,
+        connect: async () => {},
+    };
+}
 
 module.exports = redisClient;
+
+// not supported in render
+// const { createClient } = require('redis');
+// const logger = require('../utils/logger');
+// const { redis_uri } = require('./env');
+
+// const redisClient = createClient({
+//     url: redis_uri,
+// })
+
+// redisClient.on("connect", () => {
+//     logger.info("Redis Connected Successfully");
+// })
+
+// redisClient.on("error", (err) => {
+//     logger.error("Redis Connection Error: ", err);
+// });
+
+// module.exports = redisClient;
